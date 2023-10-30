e71854bab942c1978769156a0434d9adc7fe2d7b
BerkOzerr <146490036+BerkOzerr@users.noreply.github.com>
2023-10-30 15:11:58 +0300

    store api lesson finish

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,1 +1,29 @@
-console.log('04 Store API')
+require("dotenv").config();
+require("express-async-errors");
+const express = require("express");
+const app = express();
+const connectDb = require("./db/connect");
+const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+const productsRouter = require("./routes/products");
+
+app.use(express.json());
+
+app.use("/api/v1/products", productsRouter);
+
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.port || 5000; //05.06.00
+const start = async () => {
+  try {
+    await connectDb(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`Project use port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
